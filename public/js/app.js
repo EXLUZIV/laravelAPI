04f16a43475b90820fc26a0752b4c13f6dbcd948
EXLUZIV <pegasfasmain.gmail.com>
2022-02-20 16:31:52 +0200

    fix reload/change post

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,8 @@
 let id = null;
 
 async function getPosts() {
-	let res = await fetch('http://localhost/oop_connector/index.php/posts');
+
+	let res = await fetch('http://php.docker.localhost:8000/api/v1/posts');
 	let posts = await res.json();
 
 	document.querySelector('.all-box').innerHTML = '';
@@ -10,53 +11,54 @@ async function getPosts() {
 		document.querySelector('.all-box').innerHTML += `
 		<div class="box">
 			<p>Post title - ${post.post_title}</p>
-			<p id="user">User - ${post.user_id} </p>
-			<p>Content - ${post.post_text}</p>
-			<a href="#" onclick="removePost(${post.post_id})">Delete</a>
-			<a href="#" onclick="changePost('${post.post_id}', '${post.user_id}', '${post.post_title}', '${post.post_text}')">Change post</a>
+			<p id="user">User - ${post.worker_id} </p>
+			<p>Content - ${post.post_content}</p>
+			<button onclick="removePost(${post.id})">Delete</button>
+			<button onclick="changePost('${post.id}', '${post.worker_id}', '${post.post_title}', '${post.post_content}')">Change post</button>
 		</div>
 		`
 	});
-
-	getUser();
 }
 
 async function addPost() {
+
 	const postTitle = document.getElementById('postTitle').value;
 	const userId = document.getElementById('userId').value;
 	const content = document.getElementById('content').value;
 
 	if (postTitle === '' || userId === '' || content === '') {
-		alert('Fill in all the fields');
+        alert('Fill in all the fields');
+
 		return;
 	}
 
 	let formData = new FormData();
-	formData.append('user_id', userId);
+	formData.append('worker_id', userId);
 	formData.append('post_title', postTitle);
-	formData.append('post_text', content);
+    formData.append('post_content', content);
 
-	const res = await fetch('http://localhost/oop_connector/index.php/posts', {
+	const res = await fetch('http://php.docker.localhost:8000/api/v1/posts', {
 		method: 'POST',
 		body: formData
 	});
 
 	const data = await res.json();
 
-	if (data.status === true) {
+	if (data.success === true) {
 		await getPosts();
 	}
 
 }
 
 async function removePost(id) {
-	const res = await fetch(`http://localhost/oop_connector/index.php/posts/${id}`, {
+
+	const res = await fetch(`http://php.docker.localhost:8000/api/v1/posts/${id}`, {
 		method: "DELETE"
-	});
+    });
 
 	const data = await res.json();
 
-	if (data.status === true) {
+    if (data.success === true) {
 		await getPosts();
 	}
 }
@@ -64,34 +66,40 @@ async function removePost(id) {
 
 
 async function changePost(sid, userId, title, content) {
+
 	id = sid;
 	document.getElementById('userIdChange').value = userId;
 	document.getElementById('postTitleChange').value = title;
-	document.getElementById('contentChange').value = content;
+    document.getElementById('contentChange').value = content;
 }
 
 async function updatePost() {
-	const user_id = document.getElementById('userIdChange').value;
-	const post_title = document.getElementById('postTitleChange').value;
-	const post_text = document.getElementById('contentChange').value;
+
+	const userId = document.getElementById('userIdChange').value;
+	const postTitle = document.getElementById('postTitleChange').value;
+    const postContent = document.getElementById('contentChange').value;
 
 	const data = {
-		user_id: user_id,
-		post_title: post_title,
-		post_text: post_text
-	};
+		worker_id: userId,
+		post_title: postTitle,
+		post_content: postContent
+    };
 
-	const res = await fetch(`http://localhost/oop_connector/index.php/posts/${id}`, {
-		method: "PATCH",
+    const res = await fetch(`http://php.docker.localhost:8000/api/v1/posts/${id}`, {
+        headers : {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+		method: "PUT",
 		body: JSON.stringify(data)
-	})
+    })
 
-	let resData = await res.json();
+    let resData = await res.json();
 
-	if (resData.status === true) {
+	if (resData.success === true) {
 		await getPosts();
-	}
-}
+    }
 
+}
 
 getPosts();
