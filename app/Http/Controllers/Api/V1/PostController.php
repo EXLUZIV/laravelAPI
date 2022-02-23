c692e03449d7df6c59a8f5e700987a09d874fd76
EXLUZIV <pegasfasmain.gmail.com>
2022-02-23 14:02:50 +0200

    clear code

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -4,18 +4,15 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PostRequest;
-use App\Http\Requests\PostStoreRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
-use Illuminate\Http\Response;
-use Symfony\Component\Console\Input\Input;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return App\Http\Resources\PostResource;
      */
     public function index()
     {
@@ -28,7 +25,7 @@ class PostController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return App\Models\Post $post
      */
     public function store(PostRequest $request)
     {
@@ -38,19 +35,16 @@ class PostController extends Controller
         $post->comment_id=$request->comment_id;
         $post->post_title=$request->post_title;
         $post->post_content=$request->post_content;
-        $success = $post->save();
+        $post->save();
 
-        return response()->json([
-            'success' => $success
-        ]);
-
+        return $post;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  App\Models\Post $post
+     * @return App\Http\Resources\PostResource
      */
     public function show(Post $post)
     {
@@ -63,7 +57,7 @@ class PostController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  App\Models\Post $post
      * @return \Illuminate\Http\Response
      */
     public function update(PostRequest $request, Post $post)
@@ -84,7 +78,7 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  App\Models\Post $post
      * @return \Illuminate\Http\Response
      */
     public function destroy(Post $post)
--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -2,21 +2,10 @@
 
 namespace App\Http\Requests;
 
-use App\Models\Post;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Request;
 
 class PostRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
     /**
      * Get the validation rules that apply to the request.
      *
@@ -25,8 +14,7 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            'worker_id' => 'required|max:255',
-            'comment_id' => 'required|max:255',
+            'worker_id' => 'required',
             'post_title' => 'required|max:255',
             'post_content' => 'required|max:255',
         ];
